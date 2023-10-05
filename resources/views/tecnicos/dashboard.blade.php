00e54d9f9469a786868a649a10c7022978450b5c
unknown <caiobges@hotmail.com>
2023-10-04 21:34:52 -0300

    [ADD]grid

--- resources/views/tecnicos/dashboard.blade.php
+++ resources/views/tecnicos/dashboard.blade.php
@@ -2,7 +2,19 @@
 @section("content")
 
 <div class="container">
-    
+    <div class="d-flex grid grid-cols-4 grid-rows-2 gap-4">
+        <div class="d-flex col-span-3">1
+
+        </div>
+
+        <div class="d-flex col-start-4">2
+
+        </div>
+
+        <div class="d-flex col-span-4 row-start-2">3
+
+        </div>
+    </div>
 </div>
 
 @endsection
---
+++ resources/views/tecnicos/finalizacao.blade.php
@@ -0,0 +1,63 @@
+@extends("layouts.app")
+@section("content")
+    <div class="container">
+
+        <div class="grid grid-cols-4 grid-rows-10 gap-4">
+            <div class="col-span-2 col-start-1 row-start-2">1
+            </div>
+
+            <div class="col-start-3 row-start-2">2
+            </div>
+
+            <div class="col-start-4 row-start-2">3
+            </div>
+
+            <div class="col-span-2 col-start-1 row-start-3">4
+            </div>
+
+            <div class="col-start-3 row-start-3">
+            </div>
+
+            <div class="col-start-4 row-start-3">7
+            </div>
+
+            <div class="col-span-4 col-start-1 row-start-1">8
+            </div>
+
+            <div class="col-span-4 row-start-4">9
+            </div>
+
+            <div class="col-span-2 row-start-5">10
+            </div>
+
+            <div class="col-span-2 col-start-3 row-start-5">11
+            </div>
+
+            <div class="col-span-2 row-start-6">12
+            </div>
+
+            <div class="col-span-2 col-start-3 row-start-6">13
+            </div>
+
+            <div class="col-span-2 row-start-7">14
+            </div>
+
+            <div class="col-span-2 col-start-3 row-start-7">15
+            </div>
+
+            <div class="col-span-4 row-start-8">16
+            </div>
+
+            <div class="col-span-2 row-start-9">17
+            </div>
+
+            <div class="col-span-2 col-start-3 row-start-9">18
+            </div>
+
+            <div class="col-span-4 row-start-10">19
+            </div>
+        </div>
+
+    </div>
+
+@endsection
