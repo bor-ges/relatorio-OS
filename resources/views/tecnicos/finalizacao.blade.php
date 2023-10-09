1b44eba0c3802d4b153b19f549aab6a5e390246f
CAIÃO <caiobges@hotmail.com>
2023-10-09 15:54:23 -0300

    [TEMP] descrição dos campos finalização

--- resources/views/tecnicos/finalizacao.blade.php
+++ resources/views/tecnicos/finalizacao.blade.php
@@ -4,20 +4,20 @@
     <form>
 
         <div class="grid grid-cols-4 grid-rows-5 gap-4">
-            <div class="col-span-2">1</div>
-            <div class="col-start-3">2</div>
-            <div class="col-start-4">3</div>
-            <div class="col-span-3 row-start-2">4</div>
-            <div class="col-start-4 row-start-2">7</div>
-            <div class="row-start-3">8</div>
-            <div class="row-start-3">9</div>
-            <div class="row-start-3">10</div>
-            <div class="row-start-3">11</div>
-            <div >14</div>
-            <div class="row-start-4">15</div>
-            <div class="row-start-5">16</div>
-            <div class="row-start-5">17</div>
-            <div class="col-span-2 row-start-5">18</div>
+            <div class="col-span-2">numero da OS(input)</div>
+            <div class="col-start-3">tecnico resp(select)</div>
+            <div class="col-start-4">tipo de OS(select)</div>
+            <div class="col-span-3 row-start-2">adicionar materiais(feito com JS para adicionar um por um)</div>
+            <div class="col-start-4 row-start-2">hora inicio/fim(datepicker)</div>
+            <div class="row-start-3">serviço exec de acordo com cliente </div>
+            <div class="row-start-3">sem danos ao imovel</div>
+            <div class="row-start-3">Equipamentos e rede em funcionamento</div>
+            <div class="row-start-3">Com danos ao imóvel</div>
+            <div >Serviço cobrado</div>
+            <div class="row-start-4">Serviço insento de cobrança</div>
+            <div class="row-start-5">Nome do cliente(input)</div>
+            <div class="row-start-5">Endereço do serviço(input)</div>
+            <div class="col-span-2 row-start-5">ASSINATURA DO CLIENTE</div>
         </div>
     </form>
     </div>
