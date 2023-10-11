1ab2d9e10254dc867cafe2b160f9cee64427d4e0
CAIÃO <caiobges@hotmail.com>
2023-10-11 17:10:36 -0300

    [ADD]layout view finalização

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
         <link rel="stylesheet" href="/resources/css/sidebar.css">
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.css" rel="stylesheet" />
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
@@ -38,6 +39,6 @@
                 @stack('js')
             </main>
         </div>
-
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.js"></script>
     </body>
 </html>
