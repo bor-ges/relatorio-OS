2224b614e8e99ecb51235f456da091e28db97c78
CAIÃO <caiobges@hotmail.com>
2023-10-16 16:11:49 -0300

    [ADD] inicio controller

--- app/Http/Controllers/TecnicoController.php
+++ app/Http/Controllers/TecnicoController.php
@@ -28,8 +28,45 @@ class TecnicoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'numero_os' => 'required|numeric|mas:255',
+            'tecnico_resp' => 'required|max:4294967295',
+            'inicio' => 'requires|numeric|max:255',
+            'fim' => 'requires|numeric|max:255',
+            'tipo_os' => 'required|max:255',
+            'materiais' => 'required|max:4294967295',
+            'sem_danos' => 'required|max:255',
+            'com_danos' => 'required|max:255',
+            'funcionamento' => 'required|max:255',
+            'servico_exec' => 'required|max:255',
+            'insento_cobranca' => 'required|max:255',
+            'servico_cobrado' => 'required|max:255',
+            'cli_nome' => 'required|max:255',
+            'cli_endereco' => 'required|max:255',
+            'cli_assinatura' => 'required|max:255'
+        ]);
+
+        $finaliza_os = new Finaliza_os();
+
+        $finaliza_os->numero_os = $validated['numero_os'];
+        $finaliza_os->tecnico_resp = $validated['tecnico_resp'];
+        $finaliza_os->inicio = $validated['inicio'];
+        $finaliza_os->fim = $validated['fim'];
+        $finaliza_os->tipo_os = $validated['tipo_os'];
+        $finaliza_os->materiais = $validated['materiais'];
+        $finaliza_os->sem_danos = $validated['sem_danos'];
+        $finaliza_os->com_danos = $validated['com_danos'];
+        $finaliza_os->funcionamento = $validated['funcionamento'];
+        $finaliza_os->servico_exec = $validated['servico_exec'];
+        $finaliza_os->isento_cobranca = $validated['isento_cobranca'];
+        $finaliza_os->servico_cobrado = $validated['cli_nome'];
+        $finaliza_os->cli_endereco = $validated['cli_assinatura'];
+
+        $finaliza_os->save();
+
+        return redirect()->route('dashboard');
     }
+
 
     /**
      * Display the specified resource.
